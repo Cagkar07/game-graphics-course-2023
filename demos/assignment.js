1a2484d5b8680cd0a0837b66f7264c26c7e444e5
Cagdas <cagkar07@gmail.com>
2024-02-17 19:22:15 +0200

    Update assignment.js

--- demos/assignment.js
+++ demos/assignment.js
@@ -1,6 +1,11 @@
 import PicoGL from "../node_modules/picogl/build/module/picogl.js";
 import {mat4, vec3, vec4} from "../node_modules/gl-matrix/esm/index.js";
 
+import {positions, uvs, indices} from "c:/Users/cagka/Documents/GitHub/game-graphics-course-2023/blender/diamond1.js"
+
+
+
+
 function draw(timems) {
     let time = timems / 1000;
 
